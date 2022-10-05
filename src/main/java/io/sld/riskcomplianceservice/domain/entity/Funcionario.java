1aaa020db80fec2b3ddc3a2ef191de061f17a085
vitor.rodrigues <vitor.rodrigues>
2022-10-05 02:20:17 +0100

    acerto do Mapeamento de funcionario e seu DTO

--- src/main/java/io/sld/riskcomplianceservice/domain/entity/Funcionario.java
+++ src/main/java/io/sld/riskcomplianceservice/domain/entity/Funcionario.java
@@ -107,153 +107,153 @@ public class Funcionario implements Serializable {
 
     // jhipster-needle-entity-add-field - JHipster will add fields here
 
-    public Long getId() {
-        return this.id;
-    }
-
-    public Funcionario id(Long id) {
-        this.setId(id);
-        return this;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getIdnVarFuncionario() {
-        return this.idnVarFuncionario;
-    }
-
-    public Funcionario idnVarFuncionario(String idnVarFuncionario) {
-        this.setIdnVarFuncionario(idnVarFuncionario);
-        return this;
-    }
-
-    public void setIdnVarFuncionario(String idnVarFuncionario) {
-        this.idnVarFuncionario = idnVarFuncionario;
-    }
-
-    public String getnVarNome() {
-        return this.nVarNome;
-    }
-
-    public Funcionario nVarNome(String nVarNome) {
-        this.setnVarNome(nVarNome);
-        return this;
-    }
-
-    public void setnVarNome(String nVarNome) {
-        this.nVarNome = nVarNome;
-    }
-
-    public String getnVarEmail() {
-        return this.nVarEmail;
-    }
-
-    public Funcionario nVarEmail(String nVarEmail) {
-        this.setnVarEmail(nVarEmail);
-        return this;
-    }
-
-    public void setnVarEmail(String nVarEmail) {
-        this.nVarEmail = nVarEmail;
-    }
-
-    public String getnVarDescricao() {
-        return this.nVarDescricao;
-    }
-
-    public Funcionario nVarDescricao(String nVarDescricao) {
-        this.setnVarDescricao(nVarDescricao);
-        return this;
-    }
-
-    public void setnVarDescricao(String nVarDescricao) {
-        this.nVarDescricao = nVarDescricao;
-    }
-
-    public String getIdnVarEmpresa() {
-        return this.idnVarEmpresa;
-    }
-
-    public Funcionario idnVarEmpresa(String idnVarEmpresa) {
-        this.setIdnVarEmpresa(idnVarEmpresa);
-        return this;
-    }
-
-    public void setIdnVarEmpresa(String idnVarEmpresa) {
-        this.idnVarEmpresa = idnVarEmpresa;
-    }
-
-    public String getIdnvarUsuario() {
-        return this.idnvarUsuario;
-    }
-
-    public Funcionario idnvarUsuario(String idnvarUsuario) {
-        this.setIdnvarUsuario(idnvarUsuario);
-        return this;
-    }
-
-    public void setIdnvarUsuario(String idnvarUsuario) {
-        this.idnvarUsuario = idnvarUsuario;
-    }
-
-    public Set<FuncionarioOrganograma> getFuncionarioOrganogramas() {
-        return this.funcionarioOrganogramas;
-    }
-
-    public void setFuncionarioOrganogramas(Set<FuncionarioOrganograma> funcionarioOrganogramas) {
-        if (this.funcionarioOrganogramas != null) {
-            this.funcionarioOrganogramas.forEach(i -> i.setFuncionario(null));
-        }
-        if (funcionarioOrganogramas != null) {
-            funcionarioOrganogramas.forEach(i -> i.setFuncionario(this));
-        }
-        this.funcionarioOrganogramas = funcionarioOrganogramas;
-    }
-
-    public Funcionario funcionarioOrganogramas(Set<FuncionarioOrganograma> funcionarioOrganogramas) {
-        this.setFuncionarioOrganogramas(funcionarioOrganogramas);
-        return this;
-    }
-
-    public Funcionario addFuncionarioOrganograma(FuncionarioOrganograma funcionarioOrganograma) {
-        this.funcionarioOrganogramas.add(funcionarioOrganograma);
-        funcionarioOrganograma.setFuncionario(this);
-        return this;
-    }
-
-    public Funcionario removeFuncionarioOrganograma(FuncionarioOrganograma funcionarioOrganograma) {
-        this.funcionarioOrganogramas.remove(funcionarioOrganograma);
-        funcionarioOrganograma.setFuncionario(null);
-        return this;
-    }
-
-    public Empresa getEmpresa() {
-        return this.empresa;
-    }
-
-    public void setEmpresa(Empresa empresa) {
-        this.empresa = empresa;
-    }
-
-    public Funcionario empresa(Empresa empresa) {
-        this.setEmpresa(empresa);
-        return this;
-    }
-
-    public Usuario getUsuario() {
-        return this.usuario;
-    }
-
-    public void setUsuario(Usuario usuario) {
-        this.usuario = usuario;
-    }
-
-    public Funcionario usuario(Usuario usuario) {
-        this.setUsuario(usuario);
-        return this;
-    }
+//    public Long getId() {
+//        return this.id;
+//    }
+//
+//    public Funcionario id(Long id) {
+//        this.setId(id);
+//        return this;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getIdnVarFuncionario() {
+//        return this.idnVarFuncionario;
+//    }
+//
+//    public Funcionario idnVarFuncionario(String idnVarFuncionario) {
+//        this.setIdnVarFuncionario(idnVarFuncionario);
+//        return this;
+//    }
+//
+//    public void setIdnVarFuncionario(String idnVarFuncionario) {
+//        this.idnVarFuncionario = idnVarFuncionario;
+//    }
+//
+//    public String getnVarNome() {
+//        return this.nVarNome;
+//    }
+//
+//    public Funcionario nVarNome(String nVarNome) {
+//        this.setnVarNome(nVarNome);
+//        return this;
+//    }
+//
+//    public void setnVarNome(String nVarNome) {
+//        this.nVarNome = nVarNome;
+//    }
+//
+//    public String getnVarEmail() {
+//        return this.nVarEmail;
+//    }
+//
+//    public Funcionario nVarEmail(String nVarEmail) {
+//        this.setnVarEmail(nVarEmail);
+//        return this;
+//    }
+//
+//    public void setnVarEmail(String nVarEmail) {
+//        this.nVarEmail = nVarEmail;
+//    }
+//
+//    public String getnVarDescricao() {
+//        return this.nVarDescricao;
+//    }
+//
+//    public Funcionario nVarDescricao(String nVarDescricao) {
+//        this.setnVarDescricao(nVarDescricao);
+//        return this;
+//    }
+//
+//    public void setnVarDescricao(String nVarDescricao) {
+//        this.nVarDescricao = nVarDescricao;
+//    }
+//
+//    public String getIdnVarEmpresa() {
+//        return this.idnVarEmpresa;
+//    }
+//
+//    public Funcionario idnVarEmpresa(String idnVarEmpresa) {
+//        this.setIdnVarEmpresa(idnVarEmpresa);
+//        return this;
+//    }
+//
+//    public void setIdnVarEmpresa(String idnVarEmpresa) {
+//        this.idnVarEmpresa = idnVarEmpresa;
+//    }
+//
+//    public String getIdnvarUsuario() {
+//        return this.idnvarUsuario;
+//    }
+//
+//    public Funcionario idnvarUsuario(String idnvarUsuario) {
+//        this.setIdnvarUsuario(idnvarUsuario);
+//        return this;
+//    }
+//
+//    public void setIdnvarUsuario(String idnvarUsuario) {
+//        this.idnvarUsuario = idnvarUsuario;
+//    }
+//
+//    public Set<FuncionarioOrganograma> getFuncionarioOrganogramas() {
+//        return this.funcionarioOrganogramas;
+//    }
+//
+//    public void setFuncionarioOrganogramas(Set<FuncionarioOrganograma> funcionarioOrganogramas) {
+//        if (this.funcionarioOrganogramas != null) {
+//            this.funcionarioOrganogramas.forEach(i -> i.setFuncionario(null));
+//        }
+//        if (funcionarioOrganogramas != null) {
+//            funcionarioOrganogramas.forEach(i -> i.setFuncionario(this));
+//        }
+//        this.funcionarioOrganogramas = funcionarioOrganogramas;
+//    }
+//
+//    public Funcionario funcionarioOrganogramas(Set<FuncionarioOrganograma> funcionarioOrganogramas) {
+//        this.setFuncionarioOrganogramas(funcionarioOrganogramas);
+//        return this;
+//    }
+//
+//    public Funcionario addFuncionarioOrganograma(FuncionarioOrganograma funcionarioOrganograma) {
+//        this.funcionarioOrganogramas.add(funcionarioOrganograma);
+//        funcionarioOrganograma.setFuncionario(this);
+//        return this;
+//    }
+//
+//    public Funcionario removeFuncionarioOrganograma(FuncionarioOrganograma funcionarioOrganograma) {
+//        this.funcionarioOrganogramas.remove(funcionarioOrganograma);
+//        funcionarioOrganograma.setFuncionario(null);
+//        return this;
+//    }
+//
+//    public Empresa getEmpresa() {
+//        return this.empresa;
+//    }
+//
+//    public void setEmpresa(Empresa empresa) {
+//        this.empresa = empresa;
+//    }
+//
+//    public Funcionario empresa(Empresa empresa) {
+//        this.setEmpresa(empresa);
+//        return this;
+//    }
+//
+//    public Usuario getUsuario() {
+//        return this.usuario;
+//    }
+//
+//    public void setUsuario(Usuario usuario) {
+//        this.usuario = usuario;
+//    }
+//
+//    public Funcionario usuario(Usuario usuario) {
+//        this.setUsuario(usuario);
+//        return this;
+//    }
 
     // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here
 
@@ -280,9 +280,9 @@ public class Funcionario implements Serializable {
         return "Funcionario{" +
             "id=" + getId() +
             ", idnVarFuncionario='" + getIdnVarFuncionario() + "'" +
-            ", nVarNome='" + getnVarNome() + "'" +
-            ", nVarEmail='" + getnVarEmail() + "'" +
-            ", nVarDescricao='" + getnVarDescricao() + "'" +
+            ", nVarNome='" + getNVarNome() + "'" +
+            ", nVarEmail='" + getNVarEmail() + "'" +
+            ", nVarDescricao='" + getNVarDescricao() + "'" +
             ", idnVarEmpresa='" + getIdnVarEmpresa() + "'" +
             ", idnvarUsuario='" + getIdnvarUsuario() + "'" +
             "}";
